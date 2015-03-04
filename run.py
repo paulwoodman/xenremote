207febb9900f690f495f64de0cd4f3f3501f241e
Wilhelm Babernits <wilhelm.babernits@ldbv.bayern.de>
2015-03-04 11:16:49 +0100

    Fixed help

--- run.py
+++ run.py
@@ -2,12 +2,16 @@
 
 # XenServer XAPI CL interface
 #
-# (C) 2014 Wilhelm Babernits <wbabernits@onenext.de>
+# (C) 2015 Wilhelm Babernits <wbabernits@onenext.de>
 
 import sys
 import readline
 import XenAPI
 import XenRemote
+
+# unused at the moment
+#import argparse
+
 from subprocess import call
 from configobj import ConfigObj
 
@@ -35,7 +39,7 @@ session = XenAPI.Session(conf["url"])
 try:
     session.xenapi.login_with_password(conf["user"], conf["pass"])
 except:
-    sys.stdout.write("Cannot connect to server\n")
+    sys.stdout.write("cannot connect to server\n")
     exit()
 
 remote = XenRemote.Vmcontrol(session)
@@ -83,7 +87,7 @@ while(True):
         else:
             sys.stderr.write(color.YELLOW + "all vms running\n" + color.END)
 
-    # get the status of a running virtual machine
+    # get the detailed status of a running virtual machine
     elif action=='details':
         # details should be available for all vms so we can easily use get_vms_list here
         remote.get_vms_list()
@@ -165,13 +169,18 @@ while(True):
     # show help options
     elif action=='help':
         sys.stdout.write(name + " CLI (C) 2014-2015 W. Babernits <wbabernits@onenext.de>\n\n")
-        sys.stdout.write("available commands:\nstart, startall, shutdown, shutdownall, suspend, pause, status, dmesg, help, version, exit\n\n")
+        sys.stdout.write("available commands:\n\n")
+        commands = sorted(commands)
+        for command in commands:
+            sys.stdout.write("  " + command + "\n")
+        sys.stdout.write("\n")
 
     # print xenremote version
     elif action=='version':
         print("{0}/{1}-{2}".format(name, version, release))
         remote.get_version()
 
+    # clear the terminal screen
     elif action=='clear':
         call(["clear"])
 
